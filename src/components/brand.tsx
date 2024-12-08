15a95df5e3f6b8344b33ae295d02d6d032205060
haseebnaeem94 <162363763+haseebnaeem94@users.noreply.github.com>
2024-12-09 01:47:24 +0500

    Update brand.tsx

--- src/components/brand.tsx
+++ src/components/brand.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
 import { TbTruckDelivery } from "react-icons/tb";
 import { IoIosCheckmarkCircleOutline } from "react-icons/io";
 import { LuSprout } from "react-icons/lu";
